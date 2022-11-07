ca649afcd3f70840754ac84a36f0741c111aacbd
davcodin <109524717+Davcodin@users.noreply.github.com>
2022-11-07 22:41:42 +0100

    Add files via upload

---
+++ Pintos/index.js
@@ -0,0 +1,8 @@
+document.querySelector("button").addEventListener("click", clickMe);
+function clickMe() {
+    alert("Am sure you are interested")
+};
+document.querySelector("h1").addEventListener("click", forSure);
+function forSure() {
+    alert("For more enquiries please call " + "07051322585");
+}
